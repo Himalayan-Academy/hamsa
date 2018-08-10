ccaa94a8222da0e5c92e255779bad943da60a987
andre alves garzia <andre@andregarzia.com>
2018-08-09 21:03:18 -0300

    changing search

--- backend/api/server/src/hamsa.php
+++ backend/api/server/src/hamsa.php
@@ -250,7 +250,7 @@ function image_search($query, $limit, $offset) {
     };
 
     $records = ORM::for_table('image')
-        ->raw_query("select * from image where file_missing = false and metadata->>'caption' ILIKE ?;", Array($query))
+        ->raw_query("select * from image where file_missing = false and (metadata->>'caption' ILIKE ? or jsonb_exists(metadata->'keywords',?));", Array($query, $query))
         ->find_array();
 
     return array_map($extract_metadata, $records);
