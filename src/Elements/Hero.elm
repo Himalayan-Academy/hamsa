1f26607ec8bc9825f7b95091eacad79382f2bc25
andre alves garzia <andre@andregarzia.com>
2018-08-23 20:39:01 -0300

    animation for #27

--- src/Elements/Hero.elm
+++ src/Elements/Hero.elm
@@ -1,6 +1,8 @@
 module Elements.Hero exposing (view)
 
-import Elements.Image exposing (imageWithLabel)
+-- import Elements.Image exposing (imageWithLabel)
+
+import Css exposing (..)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (..)
 import Html.Styled.Events exposing (..)
@@ -12,5 +14,44 @@ view =
     div
         [ class "hero"
         , onClick (SetRoute "#/")
+        , css
+            [ position relative
+            , Css.height (px 370)
+            ]
         ]
-        [ imageWithLabel "images/sacredness2.jpg" "" ]
+        [ figure
+            [ css
+                [ position absolute
+                , left (px 0)
+                , Css.height (px 370)
+                , backgroundColor (hex "#000")
+                ]
+            ]
+            [ img
+                [ class "hero"
+                , src "images/hero2.jpg"
+                , css
+                    [ Css.width (pct 100)
+                    , Css.maxWidth (pct 100)
+                    , overflow Css.hidden
+                    ]
+                ]
+                []
+            ]
+        , figure
+            [ css
+                [ position absolute
+                , left (px 0)
+                , Css.height (px 370)
+                , backgroundColor (hex "#005aa0")
+                ]
+            , class "fade"
+            ]
+            [ img
+                [ class "hero"
+                , src "images/hero1.jpg"
+                ]
+                []
+            , span [ class "label" ] [ text "" ]
+            ]
+        ]
