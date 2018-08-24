280b4c585c4024ca42ac75c1ae28a704de5bc0bd
andre alves garzia <andre@andregarzia.com>
2018-08-23 22:44:11 -0300

    new build with mobile search

--- src/Elements/Hero.elm
+++ src/Elements/Hero.elm
@@ -6,52 +6,92 @@ import Css exposing (..)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (..)
 import Html.Styled.Events exposing (..)
+import Json.Decode as Json
 import Types exposing (..)
+
+
+onEnter : Msg -> Attribute Msg
+onEnter msg =
+    let
+        isEnter code =
+            if code == 13 then
+                Json.succeed msg
+            else
+                Json.fail "not ENTER"
+    in
+    on "keydown" (Json.andThen isEnter keyCode)
+
+
+search : Html Msg
+search =
+    div [ classList [ ( "round-wrapper", True ), ( "is-search-box", True ) ] ]
+        [ input
+            [ class "search"
+            , placeholder "Search"
+            , onInput ChangeQuery
+            , onEnter Search
+            ]
+            []
+        , i [ class "fa fa-search", onClick Search ] []
+        ]
 
 
 view : Html Msg
 view =
-    div
-        [ class "hero"
-        , onClick (SetRoute "#/")
-        , css
-            [ position relative
-            , Css.height (px 370)
-            ]
-        ]
-        [ figure
-            [ css
-                [ position absolute
-                , left (px 0)
+    div []
+        [ div
+            [ class "hero"
+            , onClick (SetRoute "#/")
+            , css
+                [ position relative
                 , Css.height (px 370)
-                , backgroundColor (hex "#000")
                 ]
             ]
-            [ img
-                [ class "hero"
-                , src "images/hero2.jpg"
-                , css
-                    [ Css.width (pct 100)
-                    , Css.maxWidth (pct 100)
-                    , overflow Css.hidden
+            [ figure
+                [ css
+                    [ position absolute
+                    , left (px 0)
+                    , Css.height (px 370)
+                    , backgroundColor (hex "#000")
                     ]
                 ]
-                []
+                [ img
+                    [ class "hero"
+                    , src "images/hero2.jpg"
+                    , css
+                        [ Css.width (pct 100)
+                        , Css.maxWidth (pct 100)
+                        , overflow Css.hidden
+                        ]
+                    ]
+                    []
+                ]
+            , figure
+                [ css
+                    [ position absolute
+                    , left (px 0)
+                    , Css.height (px 370)
+                    , backgroundColor (hex "#005aa0")
+                    ]
+                , class "fade"
+                ]
+                [ img
+                    [ class "hero"
+                    , src "images/hero1.jpg"
+                    ]
+                    []
+                , span [ class "label" ] [ text "" ]
+                ]
             ]
-        , figure
+        , div
             [ css
-                [ position absolute
-                , left (px 0)
-                , Css.height (px 370)
-                , backgroundColor (hex "#005aa0")
+                [ displayFlex
+                , alignItems center
+                , justifyContent center
+                , backgroundColor (hex "#783441")
+                , padding (px 10)
                 ]
-            , class "fade"
+            , class "hide-in-desktop"
             ]
-            [ img
-                [ class "hero"
-                , src "images/hero1.jpg"
-                ]
-                []
-            , span [ class "label" ] [ text "" ]
-            ]
+            [ search ]
         ]
