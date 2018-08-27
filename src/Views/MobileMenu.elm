ff7104f5003da0e17ed8c4bd1edc70d95a3bdad5
andre alves garzia <andre@andregarzia.com>
2018-08-27 13:30:11 -0300

    adding "about us" to mobile menu for #29

--- src/Views/MobileMenu.elm
+++ src/Views/MobileMenu.elm
@@ -74,6 +74,7 @@ menuMenu =
         , makeSelector "Tags" "#/menu/tags"
         , makeSelector "Artists" "#/menu/artists"
         , makeSelector "Collections" "#/menu/collections"
+        , makeSelector "Abous Us" "#/info"
         ]
 
 
