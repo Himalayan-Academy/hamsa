fd6841827672fad006c042b93e1d1b4cf69f4571
Andre Alves Garzia <andre@andregarzia.com>
2019-11-01 23:12:08 +0000

    mvp

---
+++ frontend/scripts/devhap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -avz ./public/* devhap@dev.himalayanacademy.com:/mnt/ExtraStorage1/hamsa/repository/build/
---
+++ frontend/scripts/hap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -avz ./public/* himalayan@dev.himalayanacademy.com:public_html/hamsa/
